6e0af11e881c0838c0df9ee1ffb4bad4e6c175d4
Sivakumarbotsa <113580915+Sivakumarbotsa@users.noreply.github.com>
2025-07-05 12:58:05 +0530

    Update: added root route to backend server.js

--- Backend/server.js
+++ Backend/server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const dotenv = require('dotenv');
+
+dotenv.config();
 const bodyParser = require("body-parser");
 const userRouter = require('./routes/user');
 const mongoose = require("mongoose");
@@ -8,13 +10,21 @@ const cors = require("cors");
 dotenv.config();
 const app = express();
 app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("✅ Employee Registration API is working!");
+});
+
+
 mongoose.connect(process.env.MONGO_URL)
 .then(() => {
     console.log("Connected to MongoDB");
 })
 .catch((err) => {
     console.error("MongoDB connection error:", err);
+
 });
+
 
 app.use(bodyParser.json());
 app.use('/api/emp', userRouter);
